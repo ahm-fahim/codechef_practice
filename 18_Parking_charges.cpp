a3c032dc96e7ee11f7c17a9c2ea541cbd4d647b8
ahm-fahim <contactahmfahim@gmail.com>
2023-07-11 15:58:09 +0600

    18 not solved

---
+++ 18_Parking_charges.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, h; //x is the first hour , y is the extra hour , h is the needs hour
+
+    cin >> x >> y >> h;
+
+
+    return 0;
+}
