f5cb7e5ca7ac41ba2e00f4903c004feea340f1d5
ahm-fahim <contactahmfahim@gmail.com>
2023-07-12 13:37:52 +0600

    23 solved

---
+++ 23_Ezio_and_Guards.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, x, y;
+
+    cin >> t;
+
+    while(t){
+        cin >> x >> y;
+
+        if(x>=y){
+            cout << "YES\n";
+        }else{
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
