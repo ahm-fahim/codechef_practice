7ef093e7b0bbf4b2694cbd30a839624848281f38
ahm-fahim <contactahmfahim@gmail.com>
2023-07-14 11:15:59 +0600

    30 solved

---
+++ 30_AIAnalysingCode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int c;
+
+    cin >> c;
+
+    if( c <= 1000){
+        cout  << "Yes\n";
+    }else {
+        cout << "No\n";
+    }
+    return 0;
+}
+
