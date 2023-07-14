e51b39aa67c52304bd0bfe5b0cbbe9a9fc81d97d
ahm-fahim <contactahmfahim@gmail.com>
2023-07-15 00:41:26 +0600

    45 solved

--- 45_Insurance.cpp
+++ 45_Insurance.cpp
@@ -1,3 +1,22 @@
-//
-// Created by fahim on 7/15/23.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--){
+        int x , y;
+
+        cin >> x >> y;
+
+        if(x<y){
+            cout << x << endl;
+        }else if(x>y){
+            cout << y << endl;
+        }else{
+            cout << x << endl;
+        }
+    }
+    return 0;
+}
