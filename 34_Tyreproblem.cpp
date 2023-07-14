eb67b6d784500320e2ec347cda0f1ff4800bf853
ahm-fahim <contactahmfahim@gmail.com>
2023-07-14 14:51:41 +0600

    34 solved

---
+++ 34_Tyreproblem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, n, m;
+
+    cin >> t;
+
+    while(t){
+        cin >> n >> m;
+
+        cout << n*2+m*4 << endl;
+
+        t--;
+    }
+    return 0;
+}
