4f6cb50ffec8760f8af1b4a5b2b7b960755f6826
ahm-fahim <contactahmfahim@gmail.com>
2023-07-14 11:09:00 +0600

    29 solved

--- 29_Expiring_Bread.cpp
+++ 29_Expiring_Bread.cpp
@@ -1,3 +1,28 @@
-//
-// Created by fahim on 7/14/23.
-//
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+    n = number of bread     | 100   | 9 | 19
+    m = expire days         | 100   | 2 | 6
+    k = eat bread in a day  | 1     | 5 | 3
+    */
+
+    int t, n, m, k;
+
+    cin >> t ;
+
+    while(t){
+        cin >> n >> m >> k;
+
+        if( n <= m*k){
+            cout << "Yes\n";
+        }else{
+            cout << "No\n";
+        }
+
+        t--;
+    }
+
+    return 0;
+}
